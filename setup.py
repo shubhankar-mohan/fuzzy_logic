dd9d7b42c4364a065d455899a64e7ccc15731cfd
Shubhankar <shubhankar.mohan@impactanalytics.co>
2020-11-05 16:17:15 +0530

    Adding Setup file and MIT License

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fuzzylogic", # Replace with your own username
+    version="0.0.2",
+    author="Shubhankar Mohan",
+    author_email="mohanshubhankar@gmail.com",
+    description="The library provides functions for fuzzy string matching, fuzzy round-off for floats and a fuzzy "
+                "function of dividing a integer into a integer distribution according to given percentage.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+      ],
+    python_requires='>=3.5',
+    include_package_data=True,
+)
